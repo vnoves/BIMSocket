09266e69b491989272a3536b93ad6944f0d7d0b4
Pablo Derendinger <pderendinger@gmail.com>
2020-07-18 16:08:13 -0300

    updated submodules

--- Viewer/Js/StaticVariables.js
+++
@@ -1,6 +0,0 @@
-   var Selmaterial = new THREE.MeshPhongMaterial({
-  color: 0x0033cc,
-  opacity: 0.5,
-  transparent : true,    // red (can also use a CSS color string here)
-  flatShading: true,
-});
